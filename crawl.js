50db8f588b82a29d78c458cc5f48c2376deabef2
MoazAlaa7 <moazalaa4727@gmail.com>
2024-01-25 15:53:32 +0200

    getURLsFromHTML, and getURLsFromHTML tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,29 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURl) {
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  const urls = [];
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      try {
+        const urlObj = new URL(`${baseURl}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error: ${err.message}`);
+      }
+    } else {
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error: ${err.message}`);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,4 +34,5 @@ function normalizeURL(urlString) {
 }
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -19,5 +19,72 @@ test("normalizeURL Decapitalize", () => {
   const input = "http://GOOGLE.com/path/";
   const actual = normalizeURL(input);
   const expected = "google.com/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href = "http://google.com/path/">
+        google
+      </a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "http://google.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["http://google.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href = "/path/">
+        google
+      </a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "http://google.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["http://google.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href = "http://google.com/">
+        google
+      </a>
+      <a href = "/path/">
+        google/path
+      </a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "http://google.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["http://google.com/", "http://google.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invaled", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href = "invaled">
+        google
+      </a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "http://google.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
